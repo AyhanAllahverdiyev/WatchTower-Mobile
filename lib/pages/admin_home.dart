b63c17ceb4e84c91d6da2180897afcb01ec5dcb3
AyhanAllahverdiyev <aykhanallahverdizade2020@gmail.com>
2023-11-01 13:01:48 +0300

    logout

--- lib/pages/admin_home.dart
+++ lib/pages/admin_home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_build_context_synchronously, prefer_const_literals_to_create_immutables, deprecated_member_use, sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:watch_tower_flutter/pages/login.dart';
 import './admin_nfc_order.dart';
 import '../components/admin_bottom_navigation.dart';
 
@@ -12,55 +13,59 @@ class AdminHomePage extends StatefulWidget {
 }
 
 class _AdminHomePageState extends State<AdminHomePage> {
-  bool isStartSelected = false; 
+  bool isStartSelected = false;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
       },
-        child: Scaffold(
-          backgroundColor: Colors.black,
-          appBar: PreferredSize(
-              preferredSize: const Size.fromHeight(40.0),
-              child: AppBar(backgroundColor: Color.fromARGB(57, 108, 126, 241))
-            ),
-          body: SafeArea(
-            child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                
-                  children: [
-                    SizedBox(height: 250),
-                   Center(
-                     child: ElevatedButton(
-                        
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.blue,
-                          onPrimary: Colors.white,
-                          padding: EdgeInsets.all(85),
-                          shape: CircleBorder()
-                        ),
-                      onPressed: () {
-                         Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => NfcOrderPage()));
-                      },
-                      child:  Text(
-                        "Change the Order",
-                        style: TextStyle(fontSize: 20.0),
-                        ), 
-                     ),
-                   )
-                  ],
-            
-              ),
-             
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(40.0),
+            child: AppBar(backgroundColor: Color.fromARGB(57, 108, 126, 241))),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 250),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        onPrimary: Colors.white,
+                        padding: EdgeInsets.all(85),
+                        shape: CircleBorder()),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => NfcOrderPage()));
+                    },
+                    child: Text(
+                      "Change the Order",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ));
+                  },
+                  child: Text('Logout'),
+                )
+              ],
             ),
           ),
-          bottomNavigationBar:  AdminBottomAppBarWidget(),
         ),
-      
+        bottomNavigationBar: AdminBottomAppBarWidget(),
+      ),
     );
   }
 }
-
